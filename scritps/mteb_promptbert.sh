2d5e7433bf80d2225c50053a7c9489f7553819c5
leoozy <zjlnbnb@163.com>
2023-05-30 20:37:56 +0800

    Delete scritps directory

--- scritps/mteb_promptbert.sh
+++
@@ -1,7 +0,0 @@
-root=/data/users/zhangjunlei/code/simcse_official/SimCSE-main/result/
-#model_path=${root}/my-sup-simcse-roberta-large_combined_data
-echo ${model_path}
-model_path=YuxinJiang/sup-promcse-roberta-base
-CUDA_VISIBLE_DEVICES=5 python eval_mteb_prompt.py --startid 0 \
-                                          --endid -1 \
-                                          --modelpath ${model_path}
