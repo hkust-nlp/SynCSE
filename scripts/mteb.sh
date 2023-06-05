f8b45f64a82997f4b93e99f9279d41b034b7721f
leoozy <zjlnbnb@163.com>
2023-06-05 14:31:55 +0800

    Delete scripts directory

--- scripts/mteb.sh
+++
@@ -1,7 +0,0 @@
-root=/data/users/zhangjunlei/code/simcse_official/SimCSE-main/result/
-#model_path=${root}/my-sup-simcse-roberta-large_combined_data
-echo ${model_path}
-model_path=qiyuw/pcl-roberta-base
-CUDA_VISIBLE_DEVICES=5 python eval_mteb.py --startid 0 \
-                                          --endid -1 \
-                                          --modelpath ${model_path}
